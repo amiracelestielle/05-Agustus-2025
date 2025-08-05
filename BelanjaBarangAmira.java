7bd4e0fb00ebee02c1c21328ed5c8f3e1295befc
amiracelestielle <amiracelestielle@gmail.com>
2025-08-06 06:06:42 +0700

    Update BelanjaBarangAmira.java

--- BelanjaBarangAmira.java
+++ BelanjaBarangAmira.java
@@ -6,19 +6,21 @@ public class BelanjaBarangAmira {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Nama Barang1: ");
-        String namabarang1 = input.nextLine();
-
         System.out.println("Barang1: ");
         String barang1 = input.next();
-        double hargabarang1 = 34.000;
+        System.out.println("Harga Barang 1");
+        double hargabarang1 = input.nextDouble();
+
         System.out.println("Barang2: ");
         String barang2 = input.next();
-        double hargabarang2 = 7.500;
+        System.out.println("Haraga Barang 2");
+        double hargabarang2 = input.nextDouble();
         
         System.out.println("--------------------------------------------");
-        System.out.println("Barang yang dibeli: "+ barang1 + " "+ barang2);
-        System.out.println("Harga: "+ hargabarang1 + " " + hargabarang2);
+        System.out.println("Barang yang dibeli: "+ barang1 + " " + "dan" + " " + barang2);
+        System.out.println("Harga: "+ hargabarang1 + " " + "dan" + " " + hargabarang2);
         System.out.println("--------------------------------------------");
     }
 }
+
+
